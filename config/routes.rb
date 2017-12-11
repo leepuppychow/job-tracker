5f98862fe6434e86d935ccbccf5862741e02b9e5
leepuppychow <leepuppychow@gmail.com>
2017-12-10 23:26:22 -0700

    edit job test and functionality complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'jobs#index'
+
   resources :companies do
     resources :jobs
   end
---
+++ spec/features/jobs/user_sees_edit_job_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe "User can edit an existing job" do
+  it "can edit an existing job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer",
+                              level_of_interest: 70,
+                              city: "Denver",
+                              company_id: company.id)
+
+
+    visit edit_company_job_path(company, job)
+
+
+    expect(page).to have_content("Edit Job")
+    expect(find_field("job[title]").value).to eq("Developer")
+    expect(find_field("job[level_of_interest]").value).to eq("70")
+    expect(find_field("job[city]").value).to eq("Denver")
+
+    fill_in "job[title]", with: "Software Developer"
+    fill_in "job[description]", with: "So fun!"
+    fill_in "job[level_of_interest]", with: 80
+    fill_in "job[city]", with: "Seattle"
+
+    click_button "Update Job"
+
+    expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
+    expect(page).to have_content("Software Developer")
+    expect(page).to have_content("80")
+    expect(page).to have_content("Seattle")
+  end
+end
