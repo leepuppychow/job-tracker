83cb5bed61421293c44cac2da9e420b4b629970c
leepuppychow <leepuppychow@gmail.com>
2017-12-11 15:06:04 -0700

    Create new category with controller actions, new and show templates, and test done

--- db/migrate/20171211212728_add_categories_to_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoriesToJobs < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :jobs, :categories, foreign_key: true
-  end
-end
--- spec/features/categories/user_can_create_new_category_spec.rb
+++ spec/features/categories/user_can_create_new_category_spec.rb
@@ -4,12 +4,6 @@ describe "User can create a new category" do
   it "user creates new category with form and is
     redirected to that category show page" do
 
-    company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer",
-                              level_of_interest: 70,
-                              city: "Denver",
-                              company_id: company.id)
-
     visit new_category_path
 
     fill_in "category[title]", with: "Technology"
