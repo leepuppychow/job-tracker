3a77d253fe6250628f95869ecb9ae9d88aace3be
leepuppychow <leepuppychow@gmail.com>
2017-12-12 13:23:28 -0700

    Drop down menu for job category working

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe Contact do
+  describe "Validations" do
+    it "is valid with full name, position, and email" do
+    end
+
+    it "is invalid without a full name" do
+    end
+
+    it "is invalid without a position" do
+    end
+
+    it "is invalid without an email" do
+    end
+  end
+
+
+  describe "Relationships" do
+    it "belongs to a company" do
+      
+    end
+  end
+end
