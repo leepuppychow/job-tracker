55c2597b997dfdfeedebe9b4ff5fb9f13f0ce3b4
leepuppychow <leepuppychow@gmail.com>
2017-12-12 12:18:41 -0700

    Comments appearing by date created working; added logic to Job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,9 @@ class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
   belongs_to :category
-  has_many :comments 
+  has_many :comments
+
+  def comments_ordered
+    comments.order(created_at: :desc)
+  end
 end
